67406b668efab7ce2b276a794b7d6eebc5d65308
gtarim <tarim.gokhan@gmail.com>
2022-02-15 22:10:30 +0300

    [fixed] main implementation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "recognition.h"
 
 int main()
 {
-    std::cout << "here\n";
+    Recognition recognition;
+    recognition.init( "./inputs/", "operations.txt", "questions.txt", "subjects.txt", "objects.txt" );
+
+    std::string input = "";
+    while( getline(std::cin, input) )
+    {
+        auto response = recognition.recognize( input );
+        if( response.empty() )
+            response = "Unrecognized intent!";
+        std::cout << input << " => Prints (Intent: " << response << ")\n";
+    }
+
+    return EXIT_SUCCESS;
 }
